b15d7d67d0633f88b1af2ba4ed3cef61be4e6f76
Corie LeClair <corieleclair.real@gmail.com>
2020-04-22 22:01:00 -0400

    Update Test.kt

--- src/main/kotlin/kai/Test.kt
+++ src/main/kotlin/kai/Test.kt
@@ -1,11 +1,14 @@
 package kai
 
 import kai.configuration.KaiConfig
+import kai.kaibrain.KaiTrainer
 import java.io.File
 
 fun main(){
     // files from the KaiPy GUI // brain files // json files
-    KaiConfig.Kai().defineConversationDirectory(File("C:\\Users\\corie\\OneDrive\\Documents\\KaiCompanion\\documents"))
+    KaiConfig.Kai().defineProjectFolder(File("C:/Users/corie/OneDrive/Documents/kai/KaiCompanion/documents"))
+    KaiConfig.Kai().definePythonInterpreter(File("C:/Users/corie/OneDrive/Documents/kai/kai/pai/"))
+    KaiConfig.Kai().enableAutoThink(true)
 
 
     while(true){
@@ -13,11 +16,13 @@ fun main(){
 
         // defining the input for Kai to read
         KaiConfig.Kai().defineInput(readLine().toString())
+
         // running conversation systems
         KaiConfig.Kai().runConversation(true)
+
         //  printing system results!
         println("Kai:${KaiConfig.Kai().getKaiResponse()}")
-        //KaiSpeech().speak(KaiConfig.Kai().getKaiResponse())
+
         // How easy?
     }
 }
